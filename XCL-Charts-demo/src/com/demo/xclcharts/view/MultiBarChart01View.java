b2bb815587724a088e254085302828b378fdde16
XiongChuanLiang <xcl_168@aliyun.com>
2014-07-14 09:52:02 +0800

    update v1.2  MultiBarChart01View

--- XCL-Charts-demo/src/com/demo/xclcharts/view/MultiBarChart01View.java
+++ XCL-Charts-demo/src/com/demo/xclcharts/view/MultiBarChart01View.java
@@ -1,3 +1,24 @@
+/**
+ * Copyright 2014  XCL-Charts
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 	
+ * @Project XCL-Charts 
+ * @Description Android图表基类库演示
+ * @author XiongChuanLiang<br/>(xcl_168@aliyun.com)
+ * @license http://www.apache.org/licenses/  Apache v2 License
+ * @version 1.0
+ */
 package com.demo.xclcharts.view;
 
 import java.text.DecimalFormat;
@@ -19,6 +40,13 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
+
+
+/**
+ * @ClassName MultiBarChart01View
+ * @Description  展示柱形图的定制性
+ * @author XiongChuanLiang<br/>(xcl_168@aliyun.com)
+ */
 
 public class MultiBarChart01View extends TouchView {
 	
@@ -117,7 +145,7 @@ public class MultiBarChart01View extends TouchView {
 			chart2.getBar().setBarStyle(XEnum.BarStyle.FILL);			
 			
 			//标题
-			chart2.setTitle("负债率标准备: 40%~60%");
+			chart2.setTitle("负债率标准: 40%~60%");
 			chart2.addSubtitle("(XCL-Charts Demo)");	
 			chart2.getPlotTitle().getTitlePaint().setColor(axisColor);
 			chart2.getPlotTitle().getSubtitlePaint().setColor(axisColor);
